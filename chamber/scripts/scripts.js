5d4c4d2378a3f41d9b84c956ea4f4fc5af9e134c
rogerbk <rogercr2002@hotmail.com>
2024-05-11 20:31:16 -0600

    W03 Project Milestone

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,9 +1,32 @@
-document.querySelector('#lastModified').textContent = "Last Modification: "+document.lastModified;
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('#lastModified').textContent = "Last Modification: " + document.lastModified;
 
-const menubutton = document.querySelector("#menu-button");
-const menuitems = document.querySelector("nav ul");
+  const menubutton = document.querySelector("#menu-button");
+  const menuitems = document.querySelector("nav ul");
 
-menubutton.addEventListener("click", () => {
-	menubutton.classList.toggle("open");
-  menuitems.classList.toggle("open");
+  menubutton.addEventListener("click", () => {
+      menubutton.classList.toggle("open");
+      menuitems.classList.toggle("open");
+  });
+
+  let storedDate = localStorage.getItem('lastVisitTime');
+  const now = Date.now();
+  localStorage.setItem('lastVisitTime', now);
+
+  if (storedDate == null) {
+      storedDate = now;
+  }
+
+  const msInHour = 1000 * 60 * 60;
+  const difference = (now - storedDate) / msInHour;
+  let message;
+
+  if (difference > 1) {
+      message = `It has been ${Math.floor(difference)} hours since your last visit`;
+  } else {
+      message = `Welcome to my website`;
+  }
+
+  document.querySelector('#visitCount').textContent = message;  
+  
 });
